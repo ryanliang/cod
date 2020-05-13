c6d2e27d1253f786a23995315218216dafa67105
Ryan Liang <ryanliang@gmail.com>
2020-05-12 23:39:06 -0400

    Two Sum slow solution.

--- leetcode/alg/TwoSum/Program.cs
+++ leetcode/alg/TwoSum/Program.cs
@@ -6,16 +6,30 @@ namespace TwoSum
     {
         static void Main(string[] args)
         {
-            var input1 = new int[] {1,2,3};
-            var target1 = 3;
-
-            var result = TwoSum(input1, target1);
-            Console.WriteLine("Hello World!");
+            TwoSum(new int[]{1,2,3}, 3);
+            TwoSum(new int[]{1,2,3,4}, 5);
+            TwoSum(new int[]{1,2,7,4}, 11);
+            TwoSum(new int[]{3,3}, 6);
         }
 
-        public static int[] TwoSum(int[] inputs, int target)
+        public static int[] TwoSum(int[] input, int target)
         {
             var result = new int[2];
+            for(int i = 0; i < input.Length - 1; i++)
+            {
+                for(int j = i + 1; j < input.Length; j++)
+                {
+                    if (input[i] + input[j] == target)
+                    {
+                        result[0] = i;
+                        result[1] = j;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Result is {result[0]} and {result[1]}");
+
             return result;
         }
     }
