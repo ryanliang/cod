2e9cca00b238c508c63aa2e0d8af8d7e946e75db
Ryan Liang <ryanliang@gmail.com>
2020-05-13 17:11:55 -0400

    FirstBadVersion: Recurssion

---
+++ leetcode/alg/FirstBadVersion/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FirstBadVersion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FirstBadVersion(5));
+
+        }
+
+        static bool IsBadVersion(int version)
+        {
+            return version == 4;
+        }
+
+        static int FirstBadVersion(int n)
+        {
+            return FirstBadVersionBetween(1, n);
+        }
+
+        static int FirstBadVersionBetween(int start, int end)
+        {
+            if (end - start == 1)
+            {
+                return IsBadVersion(start) ? start : end;
+            }
+
+            var middle = (end+start+1)/2;
+
+            return IsBadVersion(middle) ?
+                FirstBadVersionBetween(start, middle) :
+                FirstBadVersionBetween(middle + 1, end); 
+        }
+    }
+}
