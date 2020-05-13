7c58ac61a4e5d992b750e2be796c8e56caea0483
Ryan Liang <ryanliang@gmail.com>
2020-05-12 23:17:03 -0400

    wip

---
+++ leetcode/alg/TwoSum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TwoSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input1 = new int[] {1,2,3};
+            var target1 = 3;
+
+            var result = TwoSum(input1, target1);
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int[] TwoSum(int[] inputs, int target)
+        {
+            var result = new int[2];
+            return result;
+        }
+    }
+}
