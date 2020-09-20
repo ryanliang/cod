877d049dba5f90fa28f1869de4ab40a650d479b0
Ryan Liang <ryanliang@gmail.com>
2020-09-20 13:38:40 -0400

    LengthOfLongestSubstring solved

--- leetcode/alg/LengthOfLongestSubstring/Program.cs
+++ leetcode/alg/LengthOfLongestSubstring/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LengthOfLongestSubstring
 {
@@ -6,13 +7,42 @@ namespace LengthOfLongestSubstring
     {
         static void Main(string[] args)
         {
-            var s1 = "abcabcbb";
-            Console.WriteLine(LengthOfLongestSubstring(s1));
+            var s1 = "dvdf";
+            var s2 = "ou";
+            Console.WriteLine(LengthOfLongestSubstring(s1)); // 3
+            Console.WriteLine(LengthOfLongestSubstring(s2)); // 1
         }
 
         public static int LengthOfLongestSubstring(string s)
         {
-            return 0;
+            if (s == "") return 0;
+
+            var max = 1;
+            var a = s.ToCharArray();
+            var hs = new HashSet<char>();
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                var curr = 1;
+                hs.Add(a[i]);
+                for (int j = i+1; j < a.Length; j++)
+                {
+                    if(hs.Contains(a[j]))
+                    {
+                        hs.Clear();
+                        break;
+
+                    }
+                    else
+                    {
+                        curr++;
+                        hs.Add(a[j]);
+                    }
+                }
+
+                if (curr > max) max = curr;
+            } 
+            return max;
         }
     }
 }
