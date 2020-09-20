b86531dccd974117864cc68979ab3e7064f72723
Ryan Liang <ryanliang@gmail.com>
2020-09-19 22:57:13 -0400

    init LengthOfLongestSubstring

---
+++ leetcode/alg/LengthOfLongestSubstring/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LengthOfLongestSubstring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s1 = "abcabcbb";
+            Console.WriteLine(LengthOfLongestSubstring(s1));
+        }
+
+        public static int LengthOfLongestSubstring(string s)
+        {
+            return 0;
+        }
+    }
+}
