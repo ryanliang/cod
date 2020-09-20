0825714ddd774f0fb5db5cb9ce12ca963f65dbed
Ryan Liang <ryanliang@gmail.com>
2020-09-20 14:03:10 -0400

    PalindromeNumber solved

---
+++ leetcode/alg/PalindromeNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PalindromeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = 122421;
+            Console.WriteLine(IsPalindrome(num));
+        }
+
+        public static bool IsPalindrome(int x)
+        {
+            if (x < 0) return false;
+
+            var s = x.ToString();
+
+            for(int i = 0, j = s.Length -1; i <= j; i++, j--)
+            {
+                if(s[i] != s[j]) return false;
+            }
+            
+            return true;
+        }
+    }
+}
